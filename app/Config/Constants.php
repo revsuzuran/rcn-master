411b58a8f5f53b9e191485f8237ec1b17a970c71
aqil <aqilprakoso@corp.bm.co.id>
2022-12-26 07:16:50 +0700

    add base url dinamis

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -95,3 +95,7 @@ define('EVENT_PRIORITY_HIGH', 10);
 
 
 define('SITE_NAME', 'REKON CRON'); ///NAMA WEBSITE
+//base url dinamis
+$base = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) : 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']);
+
+defined('BASE') || define('BASE', $base);
